5a0e2df5d42aa6e706b05ad772bc4e2cbc0b70ee
Betim Sherifi <bs27974@seeu.edu.mk>
2021-06-19 12:41:49 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,185 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const scene = new THREE.Scene();
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMapEnabled = true;
+
+    // renderer.setClearColor(0x000,1);
+    //LIGHTS -- Ambient Light
+    const ambiColor = 0x8c8c8c;
+    const ambientLight = new THREE.AmbientLight(ambiColor);
+    scene.add(ambientLight);
+
+    // Directional Ligth
+    const dLight = new THREE.DirectionalLight(0xFFFFFF, 0.5);
+    dLight.position.set(-200, 200, -400);
+    scene.add(dLight);
+
+    //Point Light
+
+    const pLight = new THREE.PointLight(0x606060, 1.0);
+    pLight.position.set(200, 200, -400);
+    scene.add(pLight);
+
+    //Spot Light
+
+    const spotLight = new THREE.SpotLight(0xFFFFFF, 1.0);
+    spotLight.position.set(-400, 1200, 300);
+    spotLight.angle = 20 * Math.PI / 180;
+    spotLight.exponent = 1.0;
+    spotLight.target.position.set(0, 200, 0);
+    spotLight.castShadow = true;
+    scene.add(spotLight);
+
+
+    //adding stars
+    let starGeo = new THREE.Geometry();
+      for(let i=0; i< 9000; i++) {
+        let star = new THREE.Vector3(
+          Math.random() * 600 - 300,
+          Math.random() * 600 - 300,
+          Math.random() * 600 - 300
+        );
+        star.velocity = 0;
+        star.acceleration = 0.02;
+        starGeo.vertices.push(star);
+      }
+      
+    let sprite = new THREE.TextureLoader().load( 'images/star.png' );
+    let starMaterial = new THREE.ParticleBasicMaterial({
+    color: 0xaaaaaa,
+    size: 1,
+    map: sprite,
+    blending: THREE.AdditiveBlending,
+    });
+
+    let stars = new THREE.ParticleSystem(starGeo,starMaterial);
+    stars.sortParticles = true;
+
+    scene.add(stars);
+
+    
+    // var radius = 100, segments = 64,
+    // material = new THREE.LineBasicMaterial( { color: 0x0000ff } ),
+    // geometry = new THREE.CircleGeometry( radius, segments );
+
+
+    // geometry.vertices.shift();
+
+    // let line =  new THREE.Line( geometry, material );
+    // line.rotation.x = 180;
+    // scene.add( line );
+
+
+
+    const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 40000);
+    camera.position.set(-300, 300, -250);
+    camera.lookAt(scene.position);
+    scene.add(camera);
+
+    const controls = new THREE.TrackballControls(camera, renderer.domElement);
+    const keyboard = new THREEx.KeyboardState();
+
+    document.getElementById("WebGL-Output").append(renderer.domElement);
+
+
+
+
+    const solarSystem = new THREE.Object3D();
+
+    createSolarSystem(solarSystem);
+    scene.add(solarSystem);
+
+
+    function createSolarSystem(solarSystem){
+        
+
+        for(let obj of celestialObjects){
+            console.log(obj);
+            let planetTexture = new THREE.ImageUtils.loadTexture(obj.src);
+            const planetMaterial = new THREE.MeshPhongMaterial({
+                map: planetTexture,
+            });
+
+            let planet = new THREE.Mesh(new THREE.SphereGeometry(obj.radius,obj.widthSegment,obj.heightSegment), planetMaterial);
+            planet.position.x = obj.cords.x;
+            planet.position.y = obj.cords.y;
+            planet.position.z = obj.cords.z;
+            solarSystem.add(planet);
+        }
+
+
+
+        // let wordlMapIMG = new THREE.ImageUtils.loadTexture(models.earth.src);
+        
+        // const earthMaterial = new THREE.MeshPhongMaterial({
+        //     map: wordlMapIMG,
+        // });
+
+        
+        // let earth = new THREE.Mesh(new THREE.SphereGeometry(15,32,32), earthMaterial);
+        // earth.position.x = 2;
+        // earth.position.y = 95;
+        // earth.position.z = 5;
+        // solarSystem.add(earth);
+
+
+        solarSystem.traverse(function(object){
+            if(object instanceof THREE.Mesh){
+                object.castShadow = true;
+                object.receiveShadow = true;
+            }
+        })
+
+    }
+
+    window.addEventListener("resize", onWindowResize, false);
+
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }    
+
+    animate();
+
+    
+
+    function animate(){
+        requestAnimationFrame(animate);
+        render();
+        update();
+    }
+
+    function update(){
+        if(keyboard.pressed("left")){
+            solarSystem.rotation.y -= 0.02;
+        }
+        if(keyboard.pressed("right")){
+            solarSystem.rotation.y += 0.02;
+        }
+        if(keyboard.pressed("up")){
+            solarSystem.rotation.x -= 0.02;
+        }
+        if(keyboard.pressed("down")){
+            solarSystem.rotation.x += 0.02;
+        }
+        controls.update();
+    }
+
+    function render(){
+        starGeo.vertices.forEach(p => {
+            p.velocity += p.acceleration
+            p.y -= p.velocity;
+            
+            if (p.y < -200) {
+              p.y = 200;
+              p.velocity = 0;
+            }
+          });
+          starGeo.verticesNeedUpdate = true;
+          stars.rotation.y +=0.002;
+        renderer.render(scene, camera);
+    }
+})
---
+++ models.js
@@ -0,0 +1,24 @@
+let celestialObjects = [
+    {
+        src: "images/sun.png",
+        cords: {x:0, y:0, z:0},
+        radius: 40,
+        widthSegment: 32,
+        heightSegment: 32
+    },
+    {
+        src: "images/earth.jpg",
+        cords: {x:-140, y:30, z:100},
+        radius: 15,
+        widthSegment: 12,
+        heightSegment: 12
+    },
+    {
+        src: "images/mars.jfif",
+        cords: {x:-90, y:30, z:100},
+        radius: 20,
+        widthSegment: 18,
+        heightSegment: 18
+    }
+]
+
